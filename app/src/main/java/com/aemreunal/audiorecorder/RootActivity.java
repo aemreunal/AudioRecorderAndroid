4e73292cf7d5f84b7e719f7f8ca08f062ec62641
A. Emre Ünal <aemreunal@gmail.com>
2014-07-14 13:56:50 +0300

    Record successfully

--- app/src/main/java/com/aemreunal/audiorecorder/RootActivity.java
+++ app/src/main/java/com/aemreunal/audiorecorder/RootActivity.java
@@ -6,18 +6,33 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class RootActivity extends Activity {
-    private static final int DURATION = 5;
+    public static final int AUDIO_ENCODING_BIT_RATE = 128000;
+    public static final int AUDIO_SAMPLING_RATE = 16000;
+    public static final int TIMER_UPDATE_FREQUENCY_MS = 200;
+
+    private Button recordButton;
+    private EditText durationTextField;
+    private EditText jobNameTextField;
+
     private MediaRecorder recorder;
     private String outputFilePath;
     private File outputFile;
-    private Button recordButton;
+
+    private Timer timer;
+    private long durationInMS;
+    private long elapsedTime;
+
     private boolean isRecording = false;
 
     @Override
@@ -25,12 +40,71 @@ public class RootActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
         recordButton = (Button) findViewById(R.id.recordButton);
+        durationTextField = (EditText) findViewById(R.id.durationEditText);
+        jobNameTextField = (EditText) findViewById(R.id.jobNameEditText);
+    }
+
+    private void createMediaRecorder() {
         setOutputFilePath();
-        createMediaRecorder();
+        createOutputFile();
+        recorder = new MediaRecorder();
+        setRecorderSettings();
+    }
+
+    private void createTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                elapsedTime += TIMER_UPDATE_FREQUENCY_MS;
+                System.out.println("Timer fired! Current time: " + elapsedTime);
+                if (elapsedTime >= durationInMS) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            stopRecording();
+                            timer.cancel();
+                        }
+                    });
+                    System.out.println("Stopping timer.");
+                }
+            }
+        }, TIMER_UPDATE_FREQUENCY_MS, TIMER_UPDATE_FREQUENCY_MS);
     }
 
     private void setOutputFilePath() {
-        outputFilePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath() + "/test.mp4";
+        Calendar calendar = Calendar.getInstance();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(Environment.getExternalStorageDirectory().getAbsolutePath());
+        stringBuilder.append("/");
+
+        String jobName = jobNameTextField.getText().toString();
+        if (jobName == null || jobName == "" || jobName.length() == 0) {
+            jobName = "job";
+        }
+        stringBuilder.append(jobName);
+        stringBuilder.append("-android-");
+
+        stringBuilder.append(calendar.get(Calendar.YEAR));
+        stringBuilder.append("-");
+        stringBuilder.append(calendar.get(Calendar.MONTH));
+        stringBuilder.append("-");
+        stringBuilder.append(calendar.get(Calendar.DAY_OF_MONTH));
+        stringBuilder.append("-");
+        stringBuilder.append(calendar.get(Calendar.HOUR));
+        stringBuilder.append("-");
+        stringBuilder.append(calendar.get(Calendar.MINUTE));
+        stringBuilder.append("-");
+        stringBuilder.append(calendar.get(Calendar.SECOND));
+
+        stringBuilder.append(".mp4");
+
+        outputFilePath = stringBuilder.toString();
+        System.out.println("Output file path set to: " + outputFilePath);
+    }
+
+    private void createOutputFile() {
         outputFile = new File(outputFilePath);
         if (outputFile.exists()) {
             System.out.println("Output file exists, deleting it.");
@@ -38,14 +112,13 @@ public class RootActivity extends Activity {
         }
     }
 
-    private void createMediaRecorder() {
-        recorder = new MediaRecorder();
+    private void setRecorderSettings() {
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-//        recorder.setMaxDuration(DURATION);
+        recorder.setAudioEncodingBitRate(AUDIO_ENCODING_BIT_RATE);
+        recorder.setAudioSamplingRate(AUDIO_SAMPLING_RATE);
         recorder.setOutputFile(outputFilePath);
-
         recorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
             @Override
             public void onInfo(MediaRecorder mr, int what, int extra) {
@@ -55,20 +128,24 @@ public class RootActivity extends Activity {
     }
 
     public void recordButtonTapped(View view) {
-        startRecording();
+        // Check duration
+        durationInMS = 5000;
+
+        if (!isRecording) {
+            createMediaRecorder();
+            startRecording();
+        } else {
+            stopRecording();
+        }
     }
 
     private void startRecording() {
-        if (isRecording) {
-            stopRecording();
-        }
-
         try {
-            setOutputFilePath();
             recorder.prepare();
             recorder.start();
             recordButton.setText(R.string.stopRecording);
             isRecording = true;
+            createTimer();
         } catch (IllegalStateException e) {
             System.err.println("Unable to prepare recorder! IllegalState Exception");
         } catch (IOException e) {
